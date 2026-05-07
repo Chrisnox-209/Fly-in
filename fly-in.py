4c56199f4d773bd114d8a5e07f92fcb52780a97d
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-05-07 19:11:15 +0200

    🚧 wip(MAIN): Dev en cours ...

--- fly-in.py
+++ fly-in.py
@@ -1,2 +1,30 @@
+import os
+import sys
+
+
+def txt_files() -> bool:
+    count: int = 0
+
+    for file in os.listdir("./maps"):
+        if file.endswith(".txt"):
+            count += 1
+
+    if count < 1:
+        print("[Error]: map file not found")
+        return False
+    elif count > 1:
+        print("[Error]: Please upload only one map file.")
+        return False
+    else:
+        return True
+
+
+def main() -> None:
+    if txt_files():
+        print("okk !!")
+    else:
+        sys.exit(1)
+
+
 if __name__ == "__main__":
-    pass
+    main()
