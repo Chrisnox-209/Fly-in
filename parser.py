9cf242436912ebf688ce88419116bcfc2c56009d
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-05-11 19:25:46 +0200

    🔨 refactor(PARSING): Modification de la fonction parse pour retourner plusieurs listes et ajout de elif manquant

--- parser.py
+++ parser.py
@@ -1,24 +1,18 @@
 import sys
 # from pydantic import BaseModel, Field
-# from typing import Annotated
+from typing import Any
 
 
+def create_list_optional(string: str) -> list[str | dict[str, str]]:
+    list_value: list[str | dict[str, str]] = []
+    dict_option: dict[str, str] = {}
+    key: str
+    value: str
 
-# class MapConfig(BaseModel):
-#     nb_drones: int = Field(ge=1)
-#     end_hub: tuple[
-#         Annotated[str, Field(min_length=3, pattern=r"^[^\s-]*$")],
-#         [int, int]]
+    data: str = string[:string.index("[")].strip()
+    list_value.extend(data.strip().split())
 
-
-def create_list(string: str):
-    list_value = []
-    dict_option = {}
-
-    data = string[:string.index("[")].strip()
-    list_value = data.strip().split()
-
-    option = string[string.index("[") + 1 : string.index("]")].strip()
+    option: str = string[string.index("[") + 1: string.index("]")].strip()
     for item in option.split():
         key, value = item.split("=", 1)
         dict_option[key] = value
@@ -26,55 +20,67 @@ def create_list(string: str):
     list_value.append(dict_option)
     return list_value
 
-# text = "   start_hub     :    hub    0    0    [zone=restricted color=red]"
 
-# left, right = text.split(":", 1)
-# left = left.strip()
-# print(left)
-
-
-# hub_part = right[:right.index("[")].strip()
-# print(hub_part)
-
-# tags = right[right.index("["):].strip()
-# print(tags)
-
-def parse(file: str):
-    data_maps = {}
-    list_hub = []
-    connection = []
-    nb_drones = []
+def parse(file_name: str) -> tuple[list[Any], list[Any], list[Any],
+                                   list[list[Any]], list[Any]] | list[Any]:
+    hub_start: list[Any] = []
+    hub_end: list[Any] = []
+    hub_list: list[list] = []
+    connection: list[Any] = []
+    nb_drones: list[Any] = []
     try:
-        with open(file, 'r', encoding='utf8') as file:
+        key: Any
+        data: Any
+        with open(file_name, 'r', encoding='utf8') as file:
             for i, line in enumerate(file):
                 if line.startswith("nb_drones: "):
                     data = line.strip().split(":")[-1].strip()
                     nb_drones.append(data)
-                    nb_drones.append(i)
-                    print(nb_drones)
-                # elif line.startswith("start_hub: "):
-                #     key, data = line.split(":", 1)
-                #     if "[" in data and "]" in data:
-                #         data_maps["start_hub"] = create_list(data)
-                #     else:
-                #         data_maps[key] = data.strip().split()
-                # elif line.startswith("end_hub: "):
-                #     key, data = line.split(":", 1)
-                #     if "[" in data and "]" in data:
-                #         data_maps["end_hub"] = create_list(data)
-                #     else:
-                #         data_maps[key] = data.strip().split()
-                # elif line.startswith("hub: "):
-                #     key, data = line.split(":", 1)
-                #     if "[" in data and "]" in data:
-                #         data_maps["hub"] = create_list(data)
-                #     else:
-                #         data_maps[key] = data.strip().split()
-                # elif line.startswith("connection: "):
-                #     pass
+                    nb_drones.append(i + 1)
+                elif line.startswith("start_hub: "):
+                    key, data = line.split(":", 1)
+                    if "[" in data and "]" in data:
+                        hub_start = create_list_optional(data)
+                        hub_start.append(i + 1)
+                    else:
+                        hub_start = data.strip().split()
+                        hub_start.append(i + 1)
+                elif line.startswith("end_hub: "):
+                    key, data = line.split(":", 1)
+                    if "[" in data and "]" in data:
+                        hub_end = create_list_optional(data)
+                        hub_end.append(i + 1)
+                    else:
+                        hub_end = data.strip().split()
+                        hub_end.append(i + 1)
+                elif line.startswith("hub: "):
+                    key, data = line.split(":", 1)
+                    if "[" in data and "]" in data:
+                        list_data: list[str] = []
+                        list_data = create_list_optional(data)
+                        list_data.append(i + 1)
+                        hub_list.append(list_data)
+                    else:
+                        list_data = []
+                        list_data = list_data + data.strip().split()
+                        list_data.append(i + 1)
+                        hub_list.append(list_data)
+                elif line.startswith("connection: "):
+                    key, data = line.split(":", 1)
+                    if "[" in data and "]" in data:
+                        list_data = []
+                        list_data = (
+                            list_data + create_list_optional(data))
+                        list_data.append(i + 1)
+                        connection.append(list_data)
+                    else:
+                        list_data = []
+                        list_data = list_data + data.strip().split()
+                        list_data.append(i + 1)
+                        connection.append(list_data)
                 else:
                     continue
     except (FileNotFoundError, PermissionError, ValueError) as error:
         print(f"Error: {error}")
         sys.exit(1)
-    # print(data_maps)
+    return nb_drones, hub_start, hub_end, hub_list, connection
