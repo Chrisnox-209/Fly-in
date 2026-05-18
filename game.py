5300406053c6faef98d212eeb5d7e89b2c46c5eb
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-05-18 18:18:04 +0200

    🛠 tweak(ICON): Ajout de l'icone

--- game.py
+++ game.py
@@ -1,46 +1,9 @@
 import pygame
-import cv2
-import os
 import sys
 from menu import Menu
 from parser import parse
 
-# ==========================================
-# 3. PARSING FUNCTION
-# ==========================================
 
-# def parse_map_file(path):
-
-#     # Check if file exists
-#     if not os.path.exists(path):
-#         raise Exception("Map file not found")
-
-#     # Open and read file
-#     with open(path, "r") as file:
-#         lines = file.readlines()
-
-#     # Remove "\n" at the end of lines
-#     for i in range(len(lines)):
-#         lines[i] = lines[i].strip()
-
-#     # Check if map is empty
-#     if len(lines) == 0:
-#         raise Exception("Map is empty")
-
-#     # Get size of first line
-#     first_line_length = len(lines[0])
-
-#     # Check all lines
-#     for line in lines:
-
-#         if len(line) != first_line_length:
-#             raise Exception("Map is not rectangular")
-
-#     return "Map loaded successfully."
-
-# ==========================================
-# 4. GAME CLASS
-# ==========================================
 class SimpleGame:
     def __init__(self, map_path):
         self.map_path = map_path
@@ -51,22 +14,24 @@ class SimpleGame:
 
     def draw(self, screen):
         screen.fill((20, 20, 20))
-        text_surf = self.font.render(f"GAME IN PROGRESS", True, (0, 255, 0))
-        path_surf = self.font.render(f"File: {self.map_path}", True, (255, 255, 255))
-        
+        text_surf = self.font.render("GAME IN PROGRESS", True, (0, 255, 0))
+        path_surf = self.font.render(f"File: {self.map_path}",
+                                     True, (255, 255, 255))
+
         screen.blit(text_surf, (200, 200))
         screen.blit(path_surf, (200, 350))
 
 
-# ==========================================
-# 5. MAIN MANAGER (App)
-# ==========================================
 class GameApp:
     def __init__(self):
         pygame.init()
         self.width, self.height = 2832, 1504
         self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption("Maze Game")
+
+        icon = pygame.image.load("assets/icon.png").convert_alpha()
+        pygame.display.set_icon(icon)
+
+        pygame.display.set_caption("Fly_In")
         self.clock = pygame.time.Clock()
 
         self.menu = Menu(self.width, self.height)
@@ -88,7 +53,6 @@ class GameApp:
                 if selected_map:
                     self.state = "PARSING"
                     self.map_to_load = selected_map
-
 
             elif self.state == "PARSING":
                 try:
